6562fde584301c8c2965a74c9cd8b1fde134497f
Santosh kumar <bk318710@gmail.com>
2025-02-11 09:07:04 +0530

    Optimize Climbing Stairs implementation using memoization

--- Dynamic_Programming/Fibonacci/Climbing_Stairs.java
+++ Dynamic_Programming/Fibonacci/Climbing_Stairs.java
@@ -1,21 +1,28 @@
-
-
+import java.util.Arrays;
+// memoization -o(n):- these are well optimized | recursion (2^n):- these are very slow
 public class Climbing_Stairs {
-    public static int countways(int n){
-        if(n==0){
+    public static int countways(int n, int ways[]) {
+        if (n == 0) {
             return 1;
         }
-        if(n<0){
+        if (n < 0) {
             return 0;
         }
-        return  countways(n-1) + countways(n-2);
-    }
 
+        if (ways[n] != -1) { // already calculated
+            return ways[n];
+        }
+        ways[n] = countways(n - 1, ways) + countways(n - 2, ways);
+        return ways[n];
+    }
 
     public static void main(String[] args) {
-        int n = 5;
-        System.out.println(countways(n));
-        
+        int n = 5; // n=3 -> 3 & n=4 -> 5 => 8
+        int ways[] = new int[n + 1]; // memonization
+        Arrays.fill(ways, -1);
+
+        System.out.println(countways(n, ways));
+
     }
-    
+
 }
