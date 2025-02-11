e3f0af77116481f3b635f8487e55e197fdb6270c
Santosh kumar <bk318710@gmail.com>
2025-02-11 09:38:20 +0530

    Add tabulation method for counting ways in Climbing Stairs problem

--- Dynamic_Programming/Fibonacci/Climbing_Stairs.java
+++ Dynamic_Programming/Fibonacci/Climbing_Stairs.java
@@ -16,12 +16,29 @@ public class Climbing_Stairs {
         return ways[n];
     }
 
+    //for tabulation
+    //Time complexity:-o(n)
+    public static int countwaysTab(int n){
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        //tabulation loop 
+        for(int i=1; i<=n; i++){
+            if(i == 1){
+                dp[i] = dp[i-1] + 0;
+            }else{
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         int n = 5; // n=3 -> 3 & n=4 -> 5 =>8
         int ways[] = new int[n + 1]; // memonization
         Arrays.fill(ways, -1);
 
         System.out.println(countways(n, ways));
+        System.out.println(countwaysTab(n));
 
     }
 
