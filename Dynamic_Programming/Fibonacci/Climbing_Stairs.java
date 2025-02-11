de0c7680d6d493746fb32936b61cd5b3c9f109d3
Santosh kumar <bk318710@gmail.com>
2025-02-11 08:33:16 +0530

    Add Climbing Stairs implementation with recursive solution

--- Dynamic_Programming/Fibonacci/Climbing_Stairs.java
+++ Dynamic_Programming/Fibonacci/Climbing_Stairs.java
@@ -1,5 +1,21 @@
-package Fibonacci;
+
 
 public class Climbing_Stairs {
+    public static int countways(int n){
+        if(n==0){
+            return 1;
+        }
+        if(n<0){
+            return 0;
+        }
+        return countways(n-1) + countways(n-2);
+    }
+
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(countways(n));
+        
+    }
     
 }
