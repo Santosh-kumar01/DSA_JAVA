f5317c41642a69ffabb00192d3583f0015b31c94
Santosh kumar <bk318710@gmail.com>
2025-02-25 11:29:46 +0530

    Add implementations for Catalan's Number and Count of BST problems using recursion, memoization, and tabulation methods

--- Dynamic_Programming/Matrix/Matrix_Chain_Multiplication.java
+++ Dynamic_Programming/Matrix/Matrix_Chain_Multiplication.java
@@ -1,17 +1,16 @@
-
 import java.util.*;
+
 public class Matrix_Chain_Multiplication {
-    public static int mcm(int arr[], int i, int j){
-        if(i == j){
+    public static int mcm(int arr[], int i, int j) {
+        if (i == j) {
             return 0;
         }
         int ans = Integer.MAX_VALUE;
-        for(int k=i; k<= j-1; k++){
-            int cost1 = mcm(arr,i,k);
-            int cost2 = mcm(arr,k+1,j);
+        for (int k = i; k <= j - 1; k++) {
+            int cost1 = mcm(arr, i, k);
+            int cost2 = mcm(arr, k + 1, j);
 
-           
-            int cost3 = arr[i-1]*arr[k]*arr[j];
+            int cost3 = arr[i - 1] * arr[k] * arr[j];
             int finalcost = cost1 + cost2 + cost3;
             ans = Math.min(ans, finalcost);
         }
@@ -19,36 +18,8 @@ public class Matrix_Chain_Multiplication {
     }
 
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5,4};
+        int arr[] = {1, 2, 3, 4, 5}; // Choose the correct array values
         int n = arr.length;
-        System.out.println(mcm(arr, 1, n-1));
+        System.out.println(mcm(arr, 1, n - 1));
     }
-    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
